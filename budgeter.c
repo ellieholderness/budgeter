927b2f2c056dc712108382e5cff4a8717bcdd596
Ellie <ellie.holderness@gmail.com>
2018-11-06 19:06:15 +0000

    Updated to add student loan repayments for PAYE

--- budgeter.c
+++ budgeter.c
@@ -17,6 +17,9 @@ int main()
 	int cutoff_1 = 11850;
 	int cutoff_2 = 46350;
 	int cutoff_3 = 150000;
+	
+	int student_tax_cutoff = 25000;
+	int student_tax_payment = 0;
 
     printf("Yearly salary: ");
     scanf("%d", &yearly_salary);
@@ -45,11 +48,18 @@ int main()
 		salary_after_tax = yearly_salary;
 	}
 	
+	if (yearly_salary >= student_tax_cutoff)
+	{
+		student_tax_payment = 0.09 * (yearly_salary - student_tax_cutoff);
+	}
+	
+	salary_after_tax = salary_after_tax - student_tax_payment;
 	monthly_income = salary_after_tax / 12;
 	disposable_income = monthly_income - monthly_budget;
 	months_to_goal = (savings_goal / disposable_income) + 1;
 	
 	printf("Yearly income after tax will be %d \n", salary_after_tax);
+	printf("Student loan payment per month will be %d \n", student_tax_payment/12);
 	printf("Monthly income after rent will be %d \n", disposable_income);
 	printf("Months to savings goal will be %d \n", months_to_goal);
 	
